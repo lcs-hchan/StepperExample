6af221a4b57651f665a9bc330ff164e6fb42fcbb
Hayden Chan <hchan@lcs.on.ca>
2023-11-16 08:54:28 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Hayden Chan on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
